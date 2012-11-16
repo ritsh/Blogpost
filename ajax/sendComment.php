41be8a83aea1e024e0fedcc3c987c989ce5c86fe
ritsh <ritikashetty89@gmail.com>
2012-11-16 19:47:35 +0530

    sendcomment changed

--- ajax/sendComment.php
+++ ajax/sendComment.php
@@ -3,7 +3,7 @@ include '../core/init.php';
 if(isset($_POST['comment']))
 
 {  $comment=$_POST['comment'];
-	mysql_query("INSERT into blog(comment) VALUES('".$comment."')");
+	mysql_query("INSERT into blog(comment) VALUES('".$comment. "')");
 echo "success";
 }
 
